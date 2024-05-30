641e818cdd4829dbcbcfd3e9718c4944bca3b0c2
ANKIT PANDEY <116362221+ankitpandey71@users.noreply.github.com>
2024-05-30 12:17:07 +0530

    [ADD] ant design

--- src/Todo.js
+++ src/Todo.js
@@ -1,10 +1,15 @@
 import React, { useState } from "react";
+import { Button, Input, List, Typography, Checkbox, Modal } from "antd";
+import { EditOutlined, DeleteOutlined } from "@ant-design/icons";
+
+const { Text } = Typography;
 
 const Todo = () => {
   const [todos, setTodos] = useState([]);
   const [newTodo, setNewTodo] = useState("");
   const [editingTodoId, setEditingTodoId] = useState(null);
   const [updatedTodoText, setUpdatedTodoText] = useState("");
+  const [isModalVisible, setIsModalVisible] = useState(false);
 
   const addTodo = (e) => {
     e.preventDefault();
@@ -23,11 +28,13 @@ const Todo = () => {
   const startEditing = (id, text) => {
     setEditingTodoId(id);
     setUpdatedTodoText(text);
+    setIsModalVisible(true);
   };
 
   const cancelEditing = () => {
     setEditingTodoId(null);
     setUpdatedTodoText("");
+    setIsModalVisible(false);
   };
 
   const updateTodo = (id) => {
@@ -38,6 +45,7 @@ const Todo = () => {
     );
     setEditingTodoId(null);
     setUpdatedTodoText("");
+    setIsModalVisible(false);
   };
 
   const toggleTodo = (id) => {
@@ -53,43 +61,61 @@ const Todo = () => {
   };
 
   return (
-    <div>
-      <form onSubmit={addTodo}>
-        <input
+    <div style={{ maxWidth: 600, margin: "0 auto", padding: 20 }}>
+      <form onSubmit={addTodo} style={{ display: "flex", marginBottom: 20 }}>
+        <Input
           type="text"
           value={newTodo}
           onChange={(e) => setNewTodo(e.target.value)}
+          placeholder="Add a new todo"
+          style={{ marginRight: 10 }}
         />
-        <button type="submit">Add Todo</button>
+        <Button type="primary" htmlType="submit">
+          Add Todo
+        </Button>
       </form>
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>
-            {editingTodoId === todo.id ? (
-              <div>
-                <input
-                  type="text"
-                  value={updatedTodoText}
-                  onChange={(e) => setUpdatedTodoText(e.target.value)}
-                />
-                <button onClick={() => updateTodo(todo.id)}>Save</button>
-                <button onClick={cancelEditing}>Cancel</button>
-              </div>
-            ) : (
-              <div>
-                <span
-                  style={{ textDecoration: todo.completed ? "line-through" : "none" }}
-                  onClick={() => toggleTodo(todo.id)}
-                >
-                  {todo.text}
-                </span>
-                <button onClick={() => startEditing(todo.id, todo.text)}>Edit</button>
-                <button onClick={() => deleteTodo(todo.id)}>Delete</button>
-              </div>
-            )}
-          </li>
-        ))}
-      </ul>
+      <List
+        bordered
+        dataSource={todos}
+        renderItem={(todo) => (
+          <List.Item
+            actions={[
+              <EditOutlined
+                key="edit"
+                onClick={() => startEditing(todo.id, todo.text)}
+              />,
+              <DeleteOutlined
+                key="delete"
+                onClick={() => deleteTodo(todo.id)}
+              />,
+            ]}
+          >
+            <Checkbox
+              checked={todo.completed}
+              onChange={() => toggleTodo(todo.id)}
+            />
+            <Text
+              delete={todo.completed}
+              style={{ marginLeft: 8, flex: 1 }}
+              onClick={() => toggleTodo(todo.id)}
+            >
+              {todo.text}
+            </Text>
+          </List.Item>
+        )}
+      />
+      <Modal
+        title="Edit Todo"
+        visible={isModalVisible}
+        onOk={() => updateTodo(editingTodoId)}
+        onCancel={cancelEditing}
+      >
+        <Input
+          type="text"
+          value={updatedTodoText}
+          onChange={(e) => setUpdatedTodoText(e.target.value)}
+        />
+      </Modal>
     </div>
   );
 };
